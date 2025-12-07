8da07dc97778c5ec0051370974c802e5ab54e1ef
Ivan Horvath <pillesoft@gmail.com>
2025-12-07 21:03:23 +0100

    update packages

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config(object):
     DOWNLOADED_PATH='downloaded'
 
 class DevelopmentConfig(Config):
-    FFMPEG_PATH='ffmpeg-2024-02-04-git-7375a6ca7b-full_build\\bin\\ffmpeg.exe'
+    FFMPEG_PATH='c:\\dev\\private_dev\\ffmpeg-2025-12-04-git-d6458f6a8b-full_build\\bin\\ffmpeg.exe'
 
 class ProductionConfig(Config):
     DOWNLOADED_PATH='/var/YTD/Downloaded'
